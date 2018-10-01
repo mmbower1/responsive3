cf3cc8448f6d82a5004909872c01a132718852f7
Matthew Bower <mttbwr91@gmail.com>
2018-09-30 20:38:15 -0700

    home page update

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,6 +6,6 @@ module.exports = function() {
     // HTML GET Requests. Below code handles when users "visit" a page.
     // In each of the below cases the user is shown an HTML page of content
     app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"))
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 }
--- server.js
+++ server.js
@@ -21,5 +21,6 @@ app.listen(PORT, function () {
 
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./public/home.html")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
