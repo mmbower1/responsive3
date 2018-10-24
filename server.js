d4d9a98b918c458bd708d3e549c9a60eae35a46a
Matthew Bower <mttbwr91@gmail.com>
2018-10-23 18:08:44 -0700

    updated paths

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 // Series of npm packages that we will use to give our server useful functionality
 var express = require("express");
 var bodyParser = require("body-parser");
+var app = express();
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 // This sets up the basic properties for our express server
 // Tells node that we are creating an "express" server
-var app = express();
+
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
