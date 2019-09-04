48dfd85e555bd927d0d00ae1d2d45af06eee9836
Matthew Bower <mttbwr91@gmail.com>
2019-09-04 10:43:43 -0700

    add montserrat and monarch

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     // HTML GET Requests. Below code handles when users "visit" a page.
     // In each of the below cases the user is shown an HTML page of content
     app.get("/", function(req, res) {
-        console.log("here");
+        console.log("get call to home");
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
